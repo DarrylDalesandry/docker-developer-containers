1f41d9313aa309c10f9a9686c593e83968db67dd
DarrylDalesandry <209377441+DDalesandry@users.noreply.github.com>
2025-12-09 13:42:49 -0500

    25h2 update

--- java/start-java.sh
+++ java/start-java.sh
@@ -6,3 +6,4 @@ USERNAME="dd-container"
 CONTAINER=$(docker images --filter "label=identity=$IDENTITY" -q)
 
 docker run -it --rm -v ~/.ssh:/home/$USERNAME/.ssh:ro $CONTAINER
+#docker run -it -v ~/.ssh:/home/$USERNAME/.ssh:ro $CONTAINER
--- lua/start-lua.sh
+++ lua/start-lua.sh
@@ -5,4 +5,4 @@ USERNAME="dd-container"
 
 CONTAINER=$(docker images --filter "label=identity=$IDENTITY" -q)
 
-docker run -it --rm -v ~/.ssh:/home/$USERNAME/.ssh:ro $CONTAINER
+docker run -it --rm -v ~/.ssh:/home/$USERNAME/.ssh:ro -v ~/Education:/home/$USERNAME/Education $CONTAINER
